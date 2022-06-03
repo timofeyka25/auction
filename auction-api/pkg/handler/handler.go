9b7a8cecf344c5149614312d6ee15febf596321a
Timofey Bondarenko <timofeyka.com.03@gmail.com>
2022-06-03 14:44:59 +0300

    added cors policy header

--- auction-api/pkg/handler/handler.go
+++ auction-api/pkg/handler/handler.go
@@ -13,8 +13,24 @@ func NewHandler(services *service.Service) *Handler {
 	return &Handler{services: services}
 }
 
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
+}
+
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+	router.Use(CORS())
 	auth := router.Group("/auth")
 	{
 		auth.POST("/sign-up", h.signUp)
